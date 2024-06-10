f776d0d7f09f19cacc2a4911a737fa017888a90f
znak99 <xhahekcldhkznak0113@gmail.com>
2024-06-10 23:27:01 +0900

    Add user model

---
+++ AndroidFirebaseChatEx/app/src/main/java/io/github/znak99/androidfirebasechatex/dto/User.kt
@@ -0,0 +1,12 @@
+package io.github.znak99.androidfirebasechatex.dto
+
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
+data class User(
+    val uid: String? = null,
+    val email: String? = null,
+    val username: String? = null,
+    val thumbnailPath: String? = null,
+    val friendsId: List<String>? = null
+)
