ec4feb3f26ee5da5365436373dfe6ba3048b4d4d
znak99 <xhahekcldhkznak0113@gmail.com>
2024-06-13 10:02:21 +0900

    Add back press handling

--- AndroidFirebaseChatEx/app/src/main/java/io/github/znak99/androidfirebasechatex/ui/screen/authentication/SignInActivity.kt
+++ AndroidFirebaseChatEx/app/src/main/java/io/github/znak99/androidfirebasechatex/ui/screen/authentication/SignInActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
@@ -18,6 +19,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -81,6 +83,18 @@ private fun SignInScreen(auth: FirebaseAuth, modifier: Modifier = Modifier) {
     var password by rememberSaveable { mutableStateOf("") }
     var warningMessage by rememberSaveable { mutableStateOf("") }
     var isButtonDisabled by rememberSaveable { mutableStateOf(false) }
+
+    // System dismiss action overriding
+    var backHandlingEnabled by remember { mutableStateOf(true) }
+    var backPressedTime = 0L
+    BackHandler(backHandlingEnabled) {
+        if (System.currentTimeMillis() - backPressedTime <= 400L) { // EXIT
+            context.finish()
+        } else { // SHOW TOAST
+            Toast.makeText(context, "Press again fast to exit", Toast.LENGTH_SHORT).show()
+        }
+        backPressedTime = System.currentTimeMillis()
+    }
 
     Surface(
         modifier = modifier
@@ -194,6 +208,8 @@ private fun signIn(
 
                     val intent = Intent(context, ListActivity::class.java)
                     context.startActivity(intent)
+
+                    context.finish()
 
                     setIsButtonDisabled(false)
                 } else {
--- AndroidFirebaseChatEx/app/src/main/java/io/github/znak99/androidfirebasechatex/ui/screen/chat/ListActivity.kt
+++ AndroidFirebaseChatEx/app/src/main/java/io/github/znak99/androidfirebasechatex/ui/screen/chat/ListActivity.kt
@@ -1,39 +1,55 @@
 package io.github.znak99.androidfirebasechatex.ui.screen.chat
 
+import android.app.Activity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.addCallback
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import io.github.znak99.androidfirebasechatex.ui.theme.AndroidFirebaseChatExTheme
+import io.github.znak99.androidfirebasechatex.ui.theme.AppRed
 
 class ListActivity : ComponentActivity() {
 
     private val TAG = "ChatList"
+
+    // Firebase authentication
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val callback = ListActivity().onBackPressedDispatcher.addCallback(this) {
+        // Initialize
+        auth = Firebase.auth
 
-        }.handleOnBackPressed()
-
+        // NOTE: SignInActivity UI
         setContent {
             AndroidFirebaseChatExTheme {
-                ChatListScreen(
+                ListScreen(
+                    auth = auth,
                     modifier = Modifier
                         .padding(WindowInsets.systemBars.asPaddingValues())
                         .padding(horizontal = 12.dp)
@@ -44,19 +60,33 @@ class ListActivity : ComponentActivity() {
 }
 
 @Composable
-private fun ChatListScreen(modifier: Modifier = Modifier) {
+private fun ListScreen(auth: FirebaseAuth, modifier: Modifier = Modifier) {
 
-    val context = LocalContext.current
+    val TAG = "ChatList"
+
+    val context = (LocalContext.current as Activity)
+
+    // System dismiss action overriding
+    var backHandlingEnabled by remember { mutableStateOf(true) }
+    var backPressedTime = 0L
+    BackHandler(backHandlingEnabled) {
+        if (System.currentTimeMillis() - backPressedTime <= 400L) { // EXIT
+            context.finish()
+        } else { // SHOW TOAST
+            Toast.makeText(context, "Press again fast to exit", Toast.LENGTH_SHORT).show()
+        }
+        backPressedTime = System.currentTimeMillis()
+    }
 
     Surface(
         modifier = modifier
     ) {
         Button(onClick = {
             Firebase.auth.signOut()
-            context as ListActivity
             context.finish();
         }) {
             Text(text = "sign out")
         }
+
     }
 }
